175e82a38a85ebcacdee65eb6818c790d96a07d7
pv-h <p.hanzin@potok-bio.ru>
2019-11-10 01:13:02 +0400

    add 2 button

--- app/src/main/java/com/example/example1/MainActivity.kt
+++ app/src/main/java/com/example/example1/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.example1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
+import android.widget.Toast
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun toastMe1(view: View){
+        val myToast1 = Toast.makeText(this, R.string.correct_answer, Toast.LENGTH_LONG)
+        myToast1.show()
+    }
+
+    fun toastMe2(view: View){
+        val myToast1 = Toast.makeText(this, R.string.incorrect_answer, Toast.LENGTH_LONG)
+        myToast1.show()
+    }
+
 }
